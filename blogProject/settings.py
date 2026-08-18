90fbc5e7b089ca613edec2f272ce733b271e2de3
Paramdeep Singh <85700728+paramdeepsingh8313@users.noreply.github.com>
2026-08-18 22:53:53 +0530

    Set DEBUG to True in settings.py
    Changed DEBUG setting to True for development.

--- blogProject/settings.py
+++ blogProject/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-v)$+*d30g$iv0++ap+gycmk!%z44h+13-dt@-^uq-z(ng643y%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "django-log-post.vercel.app",

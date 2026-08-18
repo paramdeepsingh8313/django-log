a07db22532277e11c65a4cbcbb88331e17e42342
Paramdeep Singh <85700728+paramdeepsingh8313@users.noreply.github.com>
2026-08-18 22:38:10 +0530

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', home),
-    path('home/', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('<id>/', views.detail, name='detail')
 ]

5f37b35234b9605b6ad0a34306f68bba66c6346a
Paramdeep Singh <85700728+paramdeepsingh8313@users.noreply.github.com>
2026-08-18 22:35:22 +0530

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', home),
     path('home/', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('<id>/', views.detail, name='detail')
